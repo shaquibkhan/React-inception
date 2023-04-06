e88cdb3fd611552ecc8925a18411911a6a66d3a7
shaquib khan <connectshaquib@gmail.com>
2023-04-07 01:56:21 +0530

    added log in & log out functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,42 +1,95 @@
-import { restaurantData, CDN_URL } from "../utils/config" 
+import { useEffect, useState } from "react"
+import { restaurantData, CDN_URL } from "../utils/config"
+import Shimmer from "./Shimmer";
 
-const RestaurantCard = ({cloudinaryImageId, name, cuisines, avgRating, costForTwo, deliveryTime}) => {
-    // const {} = restaurant.data;
-    return (
-       <div className='res-card'>
-          <img className='res-image' src={CDN_URL+cloudinaryImageId}></img>
-          <h3>{name}</h3>
-           <h4>{cuisines.join(", ")}</h4>
-           <h4>{avgRating}</h4>
-          <h4>INR{costForTwo/100} for two</h4>
-          <h4>{deliveryTime} minutes</h4>
-       </div>
-    )
+function filterData(searchText, RestrauList) {
+  const copy = RestrauList;
+  const filter = copy.filter((res) => 
+    res.data.name.includes(searchText)
+  )
+  return filter;
+}
+
+const RestaurantCard = ({ cloudinaryImageId, name, cuisines, avgRating, costForTwo, deliveryTime }) => {
+  // const {} = restaurant.data;
+  return (
+    <div className='res-card'>
+      <img className='res-image' src={CDN_URL + cloudinaryImageId}></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>INR{costForTwo / 100} for two</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  
+  const [allRestraunats, setallRestraunats] = useState([]);
+  const [filteredRestrauList, setfilteredRestrauList] = useState([]);
+  const [searchText, setsearchText] = useState("");
+  
+  useEffect(()=>{
+    // API CALL
+    restrauntApi();
+  }, []);
+
+  async function restrauntApi(){
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5952314&lng=85.08308040000001&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json);
+    setallRestraunats(json?.data?.cards[2]?.data?.data?.cards);
+    setfilteredRestrauList(json?.data?.cards[2]?.data?.data?.cards);
   }
+console.log("render")
 
-const Body= () => {
-    return (
-        <>
-        <div className='body'>
-          <div className='search'>Search</div>
-          <div className='res-container'>
-          {restaurantData.map((restaurant) =>  (
-               <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>
-             )
-           )}
-             {/* <RestaurantCard {...restaurantData[0].data}/>
+
+  return allRestraunats.length === 0 ? <Shimmer/> : (
+    <>
+
+      <div className='body'>
+        <div className='search-container'>
+          
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value)
+            }}
+          />
+          <button onClick={() => {
+            const myData = filterData(searchText, allRestraunats);
+            console.log(myData);
+            setallRestraunats(myData);
+          }}>search</button>
+
+        </div>
+        <div className="filter">
+          <button onClick={() => {
+            const filterList = allRestraunats.filter((res) => res.data.avgRating > 4);
+            setRestrauList(filterList);
+            console.log(filterList)
+          }}>Top rated restaurants</button>
+        </div>
+
+        <div className='res-container'>
+          {filteredRestrauList.map((restaurant) => (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          )
+          )}
+          {/* <RestaurantCard {...restaurantData[0].data}/>
              <RestaurantCard {...restaurantData[1].data}/>
              <RestaurantCard  {...restaurantData[2].data}/>
              <RestaurantCard  {...restaurantData[3].data}/>
              <RestaurantCard  {...restaurantData[4].data}/>
              <RestaurantCard  {...restaurantData[5].data}/> 
              Other way of writing  <RestaurantCard  name={restaurantData[0].data.name}/>*/}
-             </div>
-       </div>
-        </>
-    )
- }
+        </div>
+      </div>
+    </>
+  )
+}
 
- export default Body;
+export default Body;
 
- 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 const Title = () => {
     return <img className='logo-img' alt='img-logo' src='https://img.freepik.com/premium-vector/chef-food-restaurant-logo_7085-179.jpg'></img>
  }
  
- const Header = () => 
-     
-      (
+
+ const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+     return (
      <div className='header'>
          <Title/>
          <div className='nav-items'>
+            {
+                (isLoggedIn===true) ? <button onClick={()=> setIsLoggedIn(false)}>Log Out</button> : <button onClick={()=> setIsLoggedIn(true)}>Log In</button> 
+            }
+            
+            
          <ul>
          <li>home</li>
          
@@ -17,6 +25,6 @@ const Title = () => {
      </div>
      </div>
      )
- 
+        }
 
      export default Header;
